615306bbc57f0d7b5846a9c3749a18dccc65e616
nicolas <nicolas.martin.cano@gmail.com>
2023-04-09 17:45:32 -0300

    modified yaml

--- app/src/main/java/com/uade/screenspace/repository/TestRepository.java
+++ app/src/main/java/com/uade/screenspace/repository/TestRepository.java
@@ -1,10 +1,8 @@
 package com.uade.screenspace.repository;
 
-import com.uade.screenspace.entity.GroceryItem;
+import com.uade.screenspace.entity.Movie;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TestRepository extends MongoRepository<GroceryItem, String> {
-
-    GroceryItem findByCategory(String category);
+public interface TestRepository extends MongoRepository<Movie, String> {
 
 }
