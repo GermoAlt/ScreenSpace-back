f17b34a26ed36e02e9e494415116812938859fe1
Elva Enriquez <eenriquez@cdainfo.com>
2023-06-25 22:58:51 -0300

    healthcheck sin autorizacion

--- app/src/main/java/com/uade/screenspace/config/ApplicationSecurity.java
+++ app/src/main/java/com/uade/screenspace/config/ApplicationSecurity.java
@@ -42,6 +42,7 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
          http.authorizeRequests()
                     .antMatchers(HttpMethod.POST, "/v1/auths","/v1/users","/v1/reset-password","/v1/reset-password/confirm","/v1/users/confirm-registration","/v1/forgot-password")
                     .permitAll()
+                    .antMatchers("/actuator/**").permitAll()
                     .anyRequest().authenticated()
          .and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
